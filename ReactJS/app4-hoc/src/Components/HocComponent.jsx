d712f0641dad050fccd75efe9e7b7504d9745c9d
vmurali100 <vmurali100@gmail.com>
2024-07-27 12:40:41 +0530

    HIgher Order Components

---
+++ ReactJS/app4-hoc/src/Components/HocComponent.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+const HocComponent = (OurComponent) => {
+ 
+  const NewComponent = () => {
+    const [count, setCount] = useState(0);
+    const handleIncrement = () => {
+      setCount(count + 1);
+    };
+    const handleDecrement = () => {
+      setCount(count - 1);
+    };
+    return (
+      <OurComponent
+        handleIncrement={handleIncrement}
+        handleDecrement={handleDecrement}
+        count={count}
+      />
+    );
+  };
+
+  return NewComponent;
+};
+
+export default HocComponent;
