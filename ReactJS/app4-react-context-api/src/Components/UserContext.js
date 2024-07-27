68a6d0a1d77e05271efcad3c852925857761d632
vmurali100 <vmurali100@gmail.com>
2024-07-27 15:47:45 +0530

    Added Code

---
+++ ReactJS/app4-react-context-api/src/Components/UserContext.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+export const UserInfoContext = createContext();
+export const UserInfoContextProvider = UserInfoContext.Provider;
+export const UserInfoContextConsumer = UserInfoContext.Consumer;
