24dd0404836fcfeb921fd336cfd73592f0ca4bcb
jachuk1987 <jachu.k1212@gmail.com>
2024-08-22 13:06:59 +0530

    ADD

--- ReactJs/app16-redux-toolkit-crud/src/Store/UsersSlice.js
+++ ReactJs/app16-redux-toolkit-crud/src/Store/UsersSlice.js
@@ -1,21 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
+
 const initialState = {
-    users: [
-        { id: 1, name: "Jeswanth", age: 32, email: "jeswanth@gmail.com" },
-        { id: 2, name: "Vishal", age: 21, email: "vishal@gmail.com" },
-        { id: 3, name: "Sekhar", age: 30, email: "sekhar@gmail.com" },
-        { id: 4, name: "Nethaji", age: 23, email: "nethaji@gmail.com" },
-        { id: 5, name: "Vinod", age: 30, email: "vinod@gmail.com" }
-    ]
+  users: [
+    { id: 1, name: "Jeswanth", age: 32, email: "jeswanth@gmail.com" },
+    { id: 2, name: "Vishal", age: 21, email: "vishal@gmail.com" },
+    { id: 3, name: "Sekhar", age: 30, email: "sekhar@gmail.com" },
+    { id: 4, name: "Nethaji", age: 23, email: "nethaji@gmail.com" },
+    { id: 5, name: "Vinod", age: 30, email: "vinod@gmail.com" }
+  ]
 };
+
 const UsersSlice = createSlice({
-    name: "users",
-    reducer: {
-        deleteUserAction: (state, action) => {
-            state.users.splice(action.payload.index, 1)
-        }
+  name: "users",
+  initialState,
+  reducers: {
+    deleteUserAction: (state, action) => {
+      state.users.splice(action.payload.index, 1);
     },
-    initialState
+    addUserAction: (state, action) => {
+      state.users.push(action.payload);
+    },
+    updateUserAction: (state, action) => {
+      const { index, user } = action.payload;
+      state.users[index] = user;
+    }
+  }
 });
 
 export default UsersSlice.reducer;
